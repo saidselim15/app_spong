683848339752d23e62cada74820df955b2f0da49
saidselim15 <81254099+saidselim15@users.noreply.github.com>
2022-08-26 21:52:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,511 @@
+from ast import Pass
+from this import s
+from tkinter import S
+from unicodedata import name
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+import webbrowser
+
+Builder.load_file('my.kv')
+Builder.load_file('book.kv')
+Builder.load_file('tips.kv')
+
+
+class MenuScreen(Screen):
+    pass
+
+
+class BookScreen(Screen):
+    pass
+
+
+class Book1Screen(Screen):
+    pass
+
+
+class Book2Screen(Screen):
+    pass
+
+
+class Book3Screen(Screen):
+    pass
+
+
+class Book4Screen(Screen):
+    pass
+
+
+class Book5Screen(Screen):
+    pass
+
+
+class Book6Screen(Screen):
+    pass
+
+
+class Book7Screen(Screen):
+    pass
+
+
+class Book8Screen(Screen):
+    pass
+
+
+class Book9Screen(Screen):
+    pass
+
+
+class Book10Screen(Screen):
+    pass
+
+
+class Book11Screen(Screen):
+    pass
+
+
+class Book12Screen(Screen):
+    pass
+
+
+class Book13Screen(Screen):
+    pass
+
+
+class Book14Screen(Screen):
+    pass
+
+
+class Book15Screen(Screen):
+    pass
+
+
+class Book16Screen(Screen):
+    pass
+
+
+class Book17Screen(Screen):
+    pass
+
+
+class Book18Screen(Screen):
+    pass
+
+
+class Book19Screen(Screen):
+    pass
+
+
+class Book20Screen(Screen):
+    pass
+
+
+class Book21Screen(Screen):
+    pass
+
+
+class Book22Screen(Screen):
+    pass
+
+
+class Book23Screen(Screen):
+    pass
+
+
+class Book24Screen(Screen):
+    pass
+
+
+class Book25Screen(Screen):
+    pass
+
+
+class Book26Screen(Screen):
+    pass
+
+
+class Book27Screen(Screen):
+    pass
+
+
+class Book28Screen(Screen):
+    pass
+
+
+class Book29Screen(Screen):
+    pass
+
+
+class Book30Screen(Screen):
+    pass
+
+
+class Book31Screen(Screen):
+    pass
+
+
+class Book32Screen(Screen):
+    pass
+
+
+class Book33Screen(Screen):
+    pass
+
+
+class Book34Screen(Screen):
+    pass
+
+
+class Book35Screen(Screen):
+    pass
+
+
+class Book36Screen(Screen):
+    pass
+
+
+class Book37Screen(Screen):
+    pass
+
+
+class Book38Screen(Screen):
+    pass
+
+
+class Book39Screen(Screen):
+    pass
+
+
+class Book40Screen(Screen):
+    pass
+
+
+class Book41Screen(Screen):
+    pass
+
+
+class Book42Screen(Screen):
+    pass
+
+
+class Book43Screen(Screen):
+    pass
+
+
+class Book44Screen(Screen):
+    pass
+
+
+class Book45Screen(Screen):
+    pass
+
+
+class Book46Screen(Screen):
+    pass
+
+
+class Book47Screen(Screen):
+    pass
+
+
+class Book48Screen(Screen):
+    pass
+
+
+class Book49Screen(Screen):
+    pass
+
+
+class Book50Screen(Screen):
+    pass
+
+
+class TipsScreen(Screen):
+    pass
+
+
+class BiznetipScreen(Screen):
+    pass
+
+
+class Biznetip1Screen(Screen):
+    pass
+
+
+class Biznetip2Screen(Screen):
+    pass
+
+
+class Biznetip3Screen(Screen):
+    pass
+
+
+class Biznetip4Screen(Screen):
+    pass
+
+
+class Biznetip5Screen(Screen):
+    pass
+
+
+class Biznetip6Screen(Screen):
+    pass
+
+
+class Biznetip7Screen(Screen):
+    pass
+
+
+class Biznetip8Screen(Screen):
+    pass
+
+
+class Biznetip9Screen(Screen):
+    pass
+
+
+class Biznetip10Screen(Screen):
+    pass
+
+
+class Biznetip11Screen(Screen):
+    pass
+
+
+class Biznetip12Screen(Screen):
+    pass
+
+
+class Biznetip13Screen(Screen):
+    pass
+
+
+class Biznetip14Screen(Screen):
+    pass
+
+
+class Biznetip15Screen(Screen):
+    pass
+
+
+class Biznetip16Screen(Screen):
+    pass
+
+
+class Biznetip17Screen(Screen):
+    pass
+
+
+class Biznetip18Screen(Screen):
+    pass
+
+
+class Biznetip19Screen(Screen):
+    pass
+
+
+class Biznetip20Screen(Screen):
+    pass
+
+
+class SdevtipsScreen(Screen):
+    pass
+
+
+class Sdevtips1Screen(Screen):
+    pass
+
+
+class Sdevtips2Screen(Screen):
+    pass
+
+
+class Sdevtips3Screen(Screen):
+    pass
+
+
+class Sdevtips4Screen(Screen):
+    pass
+
+
+class Sdevtips5Screen(Screen):
+    pass
+
+
+class Sdevtips6Screen(Screen):
+    pass
+
+
+class Sdevtips7Screen(Screen):
+    pass
+
+
+class Sdevtips8Screen(Screen):
+    pass
+
+
+class Sdevtips9Screen(Screen):
+    pass
+
+
+class Sdevtips10Screen(Screen):
+    pass
+
+
+class Sdevtips11Screen(Screen):
+    pass
+
+
+class Sdevtips12Screen(Screen):
+    pass
+
+
+class Sdevtips13Screen(Screen):
+    pass
+
+
+class Sdevtips14Screen(Screen):
+    pass
+
+
+class Sdevtips15Screen(Screen):
+    pass
+
+
+class Sdevtips16Screen(Screen):
+    pass
+
+
+class Sdevtips17Screen(Screen):
+    pass
+
+
+class Sdevtips18Screen(Screen):
+    pass
+
+
+class Sdevtips19Screen(Screen):
+    pass
+
+
+class Sdevtips20Screen(Screen):
+    pass
+
+
+class SpongScreen(Screen):
+    pass
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        # self.icon = 'alpha-s-box'
+        screen = ScreenManager()
+        screen.add_widget(MenuScreen(name='menu'))
+
+        screen.add_widget(BookScreen(name='book'))
+
+        screen.add_widget(Book1Screen(name='book1'))
+        screen.add_widget(Book2Screen(name='book2'))
+        screen.add_widget(Book3Screen(name='book3'))
+        screen.add_widget(Book4Screen(name='book4'))
+        screen.add_widget(Book5Screen(name='book5'))
+        screen.add_widget(Book6Screen(name='book6'))
+        screen.add_widget(Book7Screen(name='book7'))
+        screen.add_widget(Book8Screen(name='book8'))
+        screen.add_widget(Book9Screen(name='book9'))
+        screen.add_widget(Book10Screen(name='book10'))
+        screen.add_widget(Book11Screen(name='book11'))
+        screen.add_widget(Book12Screen(name='book12'))
+        screen.add_widget(Book13Screen(name='book13'))
+        screen.add_widget(Book14Screen(name='book14'))
+        screen.add_widget(Book15Screen(name='book15'))
+        screen.add_widget(Book16Screen(name='book16'))
+        screen.add_widget(Book17Screen(name='book17'))
+        screen.add_widget(Book18Screen(name='book18'))
+        screen.add_widget(Book19Screen(name='book19'))
+        screen.add_widget(Book20Screen(name='book20'))
+        screen.add_widget(Book21Screen(name='book21'))
+        screen.add_widget(Book22Screen(name='book22'))
+        screen.add_widget(Book23Screen(name='book23'))
+        screen.add_widget(Book24Screen(name='book24'))
+        screen.add_widget(Book25Screen(name='book25'))
+        screen.add_widget(Book26Screen(name='book26'))
+        screen.add_widget(Book27Screen(name='book27'))
+        screen.add_widget(Book28Screen(name='book28'))
+        screen.add_widget(Book29Screen(name='book29'))
+        screen.add_widget(Book30Screen(name='book30'))
+        screen.add_widget(Book31Screen(name='book31'))
+        screen.add_widget(Book32Screen(name='book32'))
+        screen.add_widget(Book33Screen(name='book33'))
+        screen.add_widget(Book34Screen(name='book34'))
+        screen.add_widget(Book35Screen(name='book35'))
+        screen.add_widget(Book36Screen(name='book36'))
+        screen.add_widget(Book37Screen(name='book37'))
+        screen.add_widget(Book38Screen(name='book38'))
+        screen.add_widget(Book39Screen(name='book39'))
+        screen.add_widget(Book40Screen(name='book40'))
+        screen.add_widget(Book41Screen(name='book41'))
+        screen.add_widget(Book42Screen(name='book42'))
+        screen.add_widget(Book43Screen(name='book43'))
+        screen.add_widget(Book44Screen(name='book44'))
+        screen.add_widget(Book45Screen(name='book45'))
+        screen.add_widget(Book46Screen(name='book46'))
+        screen.add_widget(Book47Screen(name='book47'))
+        screen.add_widget(Book48Screen(name='book48'))
+        screen.add_widget(Book49Screen(name='book49'))
+        screen.add_widget(Book50Screen(name='book50'))
+
+        screen.add_widget(TipsScreen(name='tips'))
+
+        screen.add_widget(BiznetipScreen(name='biznestip'))
+        screen.add_widget(Biznetip1Screen(name='btip1'))
+        screen.add_widget(Biznetip2Screen(name='btip2'))
+        screen.add_widget(Biznetip3Screen(name='btip3'))
+        screen.add_widget(Biznetip4Screen(name='btip4'))
+        screen.add_widget(Biznetip5Screen(name='btip5'))
+        screen.add_widget(Biznetip6Screen(name='btip6'))
+        screen.add_widget(Biznetip7Screen(name='btip7'))
+        screen.add_widget(Biznetip8Screen(name='btip8'))
+        screen.add_widget(Biznetip9Screen(name='btip9'))
+        screen.add_widget(Biznetip10Screen(name='btip10'))
+        screen.add_widget(Biznetip11Screen(name='btip11'))
+        screen.add_widget(Biznetip12Screen(name='btip12'))
+        screen.add_widget(Biznetip13Screen(name='btip13'))
+        screen.add_widget(Biznetip14Screen(name='btip14'))
+        screen.add_widget(Biznetip15Screen(name='btip15'))
+        screen.add_widget(Biznetip16Screen(name='btip16'))
+        screen.add_widget(Biznetip17Screen(name='btip17'))
+        screen.add_widget(Biznetip18Screen(name='btip18'))
+        screen.add_widget(Biznetip19Screen(name='btip19'))
+        screen.add_widget(Biznetip20Screen(name='btip20'))
+
+        screen.add_widget(SdevtipsScreen(name='sdevtip'))
+        screen.add_widget(Sdevtips1Screen(name='sdtip1'))
+        screen.add_widget(Sdevtips2Screen(name='sdtip2'))
+        screen.add_widget(Sdevtips3Screen(name='sdtip3'))
+        screen.add_widget(Sdevtips4Screen(name='sdtip4'))
+        screen.add_widget(Sdevtips5Screen(name='sdtip5'))
+        screen.add_widget(Sdevtips6Screen(name='sdtip6'))
+        screen.add_widget(Sdevtips7Screen(name='sdtip7'))
+        screen.add_widget(Sdevtips8Screen(name='sdtip8'))
+        screen.add_widget(Sdevtips9Screen(name='sdtip9'))
+        screen.add_widget(Sdevtips10Screen(name='sdtip10'))
+        screen.add_widget(Sdevtips11Screen(name='sdtip11'))
+        screen.add_widget(Sdevtips12Screen(name='sdtip12'))
+        screen.add_widget(Sdevtips13Screen(name='sdtip13'))
+        screen.add_widget(Sdevtips14Screen(name='sdtip14'))
+        screen.add_widget(Sdevtips15Screen(name='sdtip15'))
+        screen.add_widget(Sdevtips16Screen(name='sdtip16'))
+        screen.add_widget(Sdevtips17Screen(name='sdtip17'))
+        screen.add_widget(Sdevtips18Screen(name='sdtip18'))
+        screen.add_widget(Sdevtips19Screen(name='sdtip19'))
+        screen.add_widget(Sdevtips20Screen(name='sdtip20'))
+
+        screen.add_widget(SpongScreen(name='spong'))
+        return screen
+
+
+DemoApp().run()
